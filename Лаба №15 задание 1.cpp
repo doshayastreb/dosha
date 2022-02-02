cdc1be2c335fd34b307e6d67af0932e58e0b8fdd
doshayastreb <98846572+doshayastreb@users.noreply.github.com>
2022-02-03 00:53:08 +0300

    Add files via upload

---
+++ Лаба №15 задание 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b;
+    int i;
+    for (i = 1; i < 6; i++) {
+        cout << "Введите " << i << " число а: ";
+        cin >> a;
+        b = a * a * a;
+        cout << "Число " << a << " в 3 степени: " << b << endl;
+        a = 0;
+    }
+    return 0;
+}
