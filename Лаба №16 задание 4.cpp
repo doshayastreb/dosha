f6cec20316aaa7e4a6f4dc1b1662d98c8ff2c4de
doshayastreb <98846572+doshayastreb@users.noreply.github.com>
2022-02-03 00:54:07 +0300

    Add files via upload

---
+++ Лаба №16 задание 4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n;
+    cout << "Введите число n: ";
+    cin >> n;
+    int a[n];
+    for (i = 1; i <= n; i++) {
+        a[i] = i;
+    }
+    cout << "Элементы в данном порядке: ";
+    cout << a[1] << " " << a[n] << " " << a[2] << " ";
+    cout << a[n - 1] << " " << a[3] << " " << a[n - 2];
+    return 0;
+}
