081a42911e94d4ea94056c547a1cc1c3f7e28096
doshayastreb <98846572+doshayastreb@users.noreply.github.com>
2022-02-03 00:46:14 +0300

    Add files via upload

---
+++ Лаба №8 задание 2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "Введите значение переменной а: ";
+    cin >> a;
+    cout << "Введите значение переменной b: ";
+    cin >> b;
+    c = a / b;
+    cout << "Количество отрезков b, умещающихся на отрезке а: " << c;
+    return 0;
+}
